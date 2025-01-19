8c8e531f68698951c37334207f5874e47f6ad5d8
vaishxer <155564861+vaishxer@users.noreply.github.com>
2025-01-19 13:17:47 +0530

    Update ytmp4.js

--- ytmp4.js
+++ ytmp4.js
@@ -1,13 +1,17 @@
-// api/ytmp4.js
-const axios = require('axios');
-
 module.exports = async (req, res) => {
-    const { url } = req.query;
-    const apiUrl = process.env.YT_API_MP4_URL; // Accessing the environment variable
-    try {
-        const response = await axios.get(`${apiUrl}?url=${url}`);
-        res.status(200).json(response.data);
-    } catch (error) {
-        res.status(500).send('Error fetching data');
+  const url = req.query.url;  // URL passed from the frontend
+
+  try {
+    const response = await fetch(`${process.env.YT_API_MP4_URL}?url=${encodeURIComponent(url)}`);
+    const data = await response.json();
+
+    if (data.data) {
+      return res.status(200).json(data);
+    } else {
+      return res.status(500).json({ error: "Failed to fetch data" });
     }
+  } catch (error) {
+    console.error("Error fetching MP4 data:", error);
+    return res.status(500).json({ error: "An error occurred while fetching MP4 data" });
+  }
 };
